176fa8d6981d1368e20978c5af59f9669b7fba09
Erik Johnson <erik.johnson@enstratus.com>
2012-11-05 15:09:37 -0600

    Fixing IP Address parsing.

--- src/main/java/org/dasein/cloud/aws/compute/EC2Instance.java
+++ src/main/java/org/dasein/cloud/aws/compute/EC2Instance.java
@@ -1685,6 +1685,10 @@ public class EC2Instance implements VirtualMachineSupport {
             else if( name.equals("privateIpAddress") ) {
                 if( attr.hasChildNodes() ) {
                     String value = attr.getFirstChild().getNodeValue();
+                    String guess = guess(value);
+                    if (guess != null) {
+                    	value = guess;
+                    }
 
                     server.setPrivateIpAddresses(new String[] { value });
                 }
@@ -1692,6 +1696,10 @@ public class EC2Instance implements VirtualMachineSupport {
             else if( name.equals("ipAddress") ) {
                 if( attr.hasChildNodes() ) {
                     String value = attr.getFirstChild().getNodeValue();
+                    String resolved = resolve(value);
+                    if (resolved != null) {
+                    	value = resolved;
+                    }
 
                     server.setPublicIpAddresses(new String[] { value });
                 }
