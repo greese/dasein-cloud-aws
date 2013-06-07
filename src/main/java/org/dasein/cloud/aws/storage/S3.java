d545b2f34d797a4dc9f08a0a348d7f6ebf6c1830
binod33 <binod.kc@enstratius.com>
2013-06-07 10:51:41 -0500

    [bugzid:3052] Handle forbidden error messages for buckets.

--- src/main/java/org/dasein/cloud/aws/storage/S3.java
+++ src/main/java/org/dasein/cloud/aws/storage/S3.java
@@ -466,8 +466,8 @@ public class S3 extends AbstractBlobStoreSupport {
                 if( e.getStatus() != HttpServletResponse.SC_NOT_FOUND ) {
                     String code = e.getCode();
 
-                    if( code != null && code.equals("AccessDenied") ) {
-                        return true;
+                    if ( e.getStatus() == HttpServletResponse.SC_FORBIDDEN){
+                    	return true;
                     }
                     if( code == null || !code.equals("NoSuchBucket") ) {
                         String message = e.getMessage();
