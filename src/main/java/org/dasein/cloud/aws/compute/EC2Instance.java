a5b3a5c5baccce23e4b075a73db06764cf20a087
cafed00d <cafed00d at hotmail>
2014-02-14 14:24:42 -0800

    Include the EBS volumes in the VirtualMachine object. Requires associated dasein-cloud-core patch.

--- src/main/java/org/dasein/cloud/aws/compute/EC2Instance.java
+++ src/main/java/org/dasein/cloud/aws/compute/EC2Instance.java
@@ -2014,6 +2014,41 @@ public class EC2Instance extends AbstractVMSupport<AWSCloud> {
           server.setProviderFirewallIds( firewalls.toArray(new String[firewalls.size()]) );
         }
       }
+      else if ( name.equals( "blockDeviceMapping" ) ) {
+         ArrayList<String> volumes = new ArrayList<String>();
+         if ( attr.hasChildNodes() ) {
+           NodeList tags = attr.getChildNodes();
+
+           for ( int j = 0; j < tags.getLength(); j++ ) {
+             Node tag = tags.item( j );
+
+             if ( tag.getNodeName().equals( "item" ) && tag.hasChildNodes() ) {
+               NodeList parts = tag.getChildNodes();
+
+               for ( int k = 0; k < parts.getLength(); k++ ) {
+                 Node part = parts.item( k );
+
+                 if ( part.getNodeName().equalsIgnoreCase( "ebs" ) ) {
+                   String volumeId = null;
+                   NodeList subparts = part.getChildNodes();
+                   for ( int l = 0; l < subparts.getLength(); l++ ) {
+                     Node subpart = subparts.item( l );
+                     if ( subpart.getNodeName().equals( "volumeId" ) && subpart.hasChildNodes() ) {
+                       volumeId = subpart.getFirstChild().getNodeValue().trim();
+                     }
+                   }
+                   if ( volumeId != null ) {
+                     volumes.add( volumeId );
+                   }
+                 }
+               }
+             }
+           }
+         }
+         if ( volumes.size() > 0 ) {
+           server.setProviderVolumeIds( volumes.toArray(new String[volumes.size()]) );
+         }
+       }
     }
       if( server.getPlatform() == null ) {
 		    server.setPlatform(Platform.UNKNOWN);
