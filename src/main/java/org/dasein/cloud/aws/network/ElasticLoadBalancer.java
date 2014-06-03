59b87ca2eba0c1e7ee2ade8b79d2feed7ee202f9
Eduard Bakaev <bakaeved@gmail.com>
2014-06-03 13:50:14 +0400

    TheWeatherCompany/grid-api#813 methods implementation

--- src/main/java/org/dasein/cloud/aws/network/ElasticLoadBalancer.java
+++ src/main/java/org/dasein/cloud/aws/network/ElasticLoadBalancer.java
@@ -1079,14 +1079,60 @@ public class ElasticLoadBalancer extends AbstractLoadBalancerSupport<AWSCloud> {
 
 	@Override
 	public void attachLoadBalancerToSubnets(@Nonnull String toLoadBalancerId, @Nonnull String... subnetIdsToAdd) throws CloudException, InternalException {
-		//TODO
-		String result = "";
+		APITrace.begin(provider, "LB.attachLoadBalancerToSubnets");
+		try {
+			ProviderContext ctx = provider.getContext();
+			if (ctx == null) {
+				throw new CloudException("No valid context is established for this request");
+			}
+
+			Map<String, String> parameters = getELBParameters(ctx, ELBMethod.ATTACH_LB_TO_SUBNETS);
+			parameters.put("LoadBalancerName", toLoadBalancerId);
+			for(int i = 1; i <= subnetIdsToAdd.length; i++) {
+				parameters.put("Subnets.member." + i, subnetIdsToAdd[i - 1] );
+			}
+
+			ELBMethod method = new ELBMethod(provider, ctx, parameters);
+			try {
+				method.invoke();
+			}
+			catch ( EC2Exception e ) {
+				logger.error(e.getSummary());
+				throw new CloudException(e);
+			}
+		}
+		finally {
+			APITrace.end();
+		}
 	}
 
 	@Override
 	public void detachLoadBalancerFromSubnets(@Nonnull String fromLoadBalancerId, @Nonnull String... subnetIdsToDelete) throws CloudException, InternalException {
-		//TODO
-		String result = "";
+		APITrace.begin(provider, "LB.detachLoadBalancerFromSubnets");
+		try {
+			ProviderContext ctx = provider.getContext();
+			if (ctx == null) {
+				throw new CloudException("No valid context is established for this request");
+			}
+
+			Map<String, String> parameters = getELBParameters(ctx, ELBMethod.DETACH_LB_FROM_SUBNETS);
+			parameters.put("LoadBalancerName", fromLoadBalancerId);
+			for(int i = 1; i <= subnetIdsToDelete.length; i++) {
+				parameters.put("Subnets.member." + i, subnetIdsToDelete[i - 1] );
+			}
+
+			ELBMethod method = new ELBMethod(provider, ctx, parameters);
+			try {
+				method.invoke();
+			}
+			catch ( EC2Exception e ) {
+				logger.error(e.getSummary());
+				throw new CloudException(e);
+			}
+		}
+		finally {
+			APITrace.end();
+		}
 	}
 
 	private LoadBalancerHealthCheck toLBHealthCheck( @Nullable String lbId, @Nonnull Node node ) {
