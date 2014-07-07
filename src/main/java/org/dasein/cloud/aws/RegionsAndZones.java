6f0821236b61b92d60d7154eb58ebab53d14220a
daniellemayne <danielle.mayne@enstratus.com>
2014-07-07 09:41:50 +0100

    added missing import statement

--- src/main/java/org/dasein/cloud/aws/RegionsAndZones.java
+++ src/main/java/org/dasein/cloud/aws/RegionsAndZones.java
@@ -26,6 +26,7 @@ import org.dasein.cloud.ProviderContext;
 import org.dasein.cloud.aws.compute.EC2Exception;
 import org.dasein.cloud.aws.compute.EC2Method;
 import org.dasein.cloud.dc.DataCenter;
+import org.dasein.cloud.dc.DataCenterCapabilities;
 import org.dasein.cloud.dc.DataCenterServices;
 import org.dasein.cloud.dc.Region;
 import org.dasein.cloud.util.APITrace;
